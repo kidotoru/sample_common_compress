ac19ab4097b5a10f3de46ebca0125ffefdfd62af
木戸 亨 <kido.toru.wood@gmail.com>
2016-05-03 06:57:31 +0900

    tabをスペースに

--- src/main/java/net/treewoods/sample_common_compress/TarUtil.java
+++ src/main/java/net/treewoods/sample_common_compress/TarUtil.java
@@ -15,60 +15,62 @@ import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.io.FileUtils;
 
 public class TarUtil {
-	
-	// 指定したディレクトリ内のファイルをアーカイブ
-	public void archive(String baseDir, String outputFile) throws FileNotFoundException, IOException {
-		// baseDir内のファイル一覧を取得
-		Collection<File> listFiles = FileUtils.listFiles(new File(baseDir), null, true);
-		String[] inputFiles = listFiles.stream().map(t -> {
-			String p = t.getPath();
-			return p.substring(baseDir.length(), p.length());
-		}).toArray(count -> {return new String[count];});
-		
-		this.archive(baseDir, inputFiles, outputFile);
-	}
 
-	//作成
-	public void archive(String baseDir, String[] inputFiles, String outputFile) throws FileNotFoundException, IOException {
-		try (TarArchiveOutputStream out = new TarArchiveOutputStream(new FileOutputStream(outputFile))) {
-			for (String inputFile : inputFiles) {
-				File f = new File(baseDir + inputFile);
-				out.putArchiveEntry(new TarArchiveEntry(f, inputFile));
-				out.write(org.apache.commons.io.FileUtils.readFileToByteArray(f));
-				out.closeArchiveEntry();
-			}
-		}
-	}
+    // 指定したディレクトリ内のファイルをアーカイブ
+    public void archive(String baseDir, String outputFile) throws FileNotFoundException, IOException {
+        // baseDir内のファイル一覧を取得
+        Collection<File> listFiles = FileUtils.listFiles(new File(baseDir), null, true);
+        String[] inputFiles = listFiles.stream().map(t -> {
+            String p = t.getPath();
+            return p.substring(baseDir.length(), p.length());
+        }).toArray(count -> {
+            return new String[count];
+        });
 
-	//展開
-	public void extract(String inputFile, String outputDir) throws FileNotFoundException, IOException {
-		try (TarArchiveInputStream in = new TarArchiveInputStream(new FileInputStream(inputFile))) {
-			BufferedInputStream bis = new BufferedInputStream(in);
-			ArchiveEntry entry;
-			while ((entry = in.getNextEntry()) != null) {
-				File outFile = new File(outputDir + "/" + entry.getName());
-				
-				// ディレクトリならディレクトリ作成
-				if (entry.isDirectory()) {
-					outFile.mkdirs();
-					continue;
-				}
-				
-				// 親のディレクトリがなければ作成
-				if (!outFile.getParentFile().exists()) {
-					outFile.getParentFile().mkdirs();
-				}
+        this.archive(baseDir, inputFiles, outputFile);
+    }
 
-				//FileUtils.copyInputStreamToFile(part.getInputStream(), file);
-				try (FileOutputStream fos = new FileOutputStream(outFile);
-						BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-					// エントリの出力
-					int i;
-					while ((i = bis.read()) != -1) {
-						bos.write(i);
-					}
-				}
-			}
-		}
-	}
+    //作成
+    public void archive(String baseDir, String[] inputFiles, String outputFile) throws FileNotFoundException, IOException {
+        try (TarArchiveOutputStream out = new TarArchiveOutputStream(new FileOutputStream(outputFile))) {
+            for (String inputFile : inputFiles) {
+                File f = new File(baseDir + inputFile);
+                out.putArchiveEntry(new TarArchiveEntry(f, inputFile));
+                out.write(org.apache.commons.io.FileUtils.readFileToByteArray(f));
+                out.closeArchiveEntry();
+            }
+        }
+    }
+
+    //展開
+    public void extract(String inputFile, String outputDir) throws FileNotFoundException, IOException {
+        try (TarArchiveInputStream in = new TarArchiveInputStream(new FileInputStream(inputFile))) {
+            BufferedInputStream bis = new BufferedInputStream(in);
+            ArchiveEntry entry;
+            while ((entry = in.getNextEntry()) != null) {
+                File outFile = new File(outputDir + "/" + entry.getName());
+
+                // ディレクトリならディレクトリ作成
+                if (entry.isDirectory()) {
+                    outFile.mkdirs();
+                    continue;
+                }
+
+                // 親のディレクトリがなければ作成
+                if (!outFile.getParentFile().exists()) {
+                    outFile.getParentFile().mkdirs();
+                }
+
+                //FileUtils.copyInputStreamToFile(part.getInputStream(), file);
+                try (FileOutputStream fos = new FileOutputStream(outFile);
+                        BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+                    // エントリの出力
+                    int i;
+                    while ((i = bis.read()) != -1) {
+                        bos.write(i);
+                    }
+                }
+            }
+        }
+    }
 }
