cb2bbfef148e156abe90ec42863c1407ca773aa6
木戸 亨 <kido.toru.wood@gmail.com>
2016-05-03 06:57:54 +0900

    tabをスペースに

--- src/test/java/net/treewoods/sample_common_compress/TarUtilTest.java
+++ src/test/java/net/treewoods/sample_common_compress/TarUtilTest.java
@@ -17,64 +17,61 @@ import static org.junit.Assert.*;
  * @author toru
  */
 public class TarUtilTest {
-	
-	public TarUtilTest() {
-	}
-	
-	@BeforeClass
-	public static void setUpClass() {
-	}
-	
-	@AfterClass
-	public static void tearDownClass() {
-	}
-	
-	@Before
-	public void setUp() {
-	}
-	
-	@After
-	public void tearDown() {
-	}
 
-	/**
-	 * Test of archive method, of class TarUtil.
-	 */
-	@Test
-	public void testArchive() throws Exception {
-		System.out.println("archive");
-		String baseDir = "./in/";
-		String[] inputFiles = {"file1","file2","dir/file3"};
-		String outputFile = "./out/test.tar";
-		TarUtil instance = new TarUtil();
-		instance.archive(baseDir,inputFiles, outputFile);
-	}
+    public TarUtilTest() {
+    }
 
-	/**
-	 * Test of extract method, of class TarUtil.
-	 */
-	@Test
-	public void testExtract() throws Exception {
-		System.out.println("extract");
-		String inputFile = "./out/test.tar";
-		String outputDir = "./out";
-		TarUtil instance = new TarUtil();
-		instance.extract(inputFile, outputDir);
-	}
+    @BeforeClass
+    public static void setUpClass() {
+    }
 
-	/**
-	 * Test of archive method, of class TarUtil.
-	 */
-	@Test
-	public void testArchive_String_String() throws Exception {
-		System.out.println("archive");
-		String baseDir = "./in/";
-		String outputFile = "./out/test.tar";
-		TarUtil instance = new TarUtil();
-		instance.archive(baseDir, outputFile);
-	}
+    @AfterClass
+    public static void tearDownClass() {
+    }
 
+    @Before
+    public void setUp() {
+    }
 
+    @After
+    public void tearDown() {
+    }
 
-	
+    /**
+     * Test of archive method, of class TarUtil.
+     */
+    @Test
+    public void testArchive() throws Exception {
+        System.out.println("archive");
+        String baseDir = "./in/";
+        String[] inputFiles = {"file1", "file2", "dir/file3"};
+        String outputFile = "./out/test.tar";
+        TarUtil instance = new TarUtil();
+        instance.archive(baseDir, inputFiles, outputFile);
+    }
+
+    /**
+     * Test of extract method, of class TarUtil.
+     */
+    @Test
+    public void testExtract() throws Exception {
+        System.out.println("extract");
+        String inputFile = "./out/test.tar";
+        String outputDir = "./out";
+        TarUtil instance = new TarUtil();
+        instance.extract(inputFile, outputDir);
+    }
+
+    /**
+     * Test of archive method, of class TarUtil.
+     */
+    @Test
+    public void testArchive_String_String() throws Exception {
+        System.out.println("archive");
+        String baseDir = "./in/";
+        String outputFile = "./out/test.tar";
+        TarUtil instance = new TarUtil();
+        instance.archive(baseDir, outputFile);
+    }
+
 }
